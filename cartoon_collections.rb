d4da20fb3ef81582961077ecb738228ec77a1f71
Bree Warren <ambreea.warren@gmail.com>
2020-05-19 08:20:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,8 +24,3 @@ def find_valid_calls(planeteer_calls)
     calls == "Earth!" || calls == "Wind!" || calls == "Fire!" || calls == "Water!" || calls == "Heart!"
   end
 end
-
-
-
-# Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
-# Return the first valid call found, or return nil if no valid calls are found
